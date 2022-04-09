2771cb7ef5b31957b35e9bd0ad296aa4a9c3c9a6
Akash rawal <rawalakash37@gmail.com>
2022-04-09 16:34:04 +0530

    3r

---
+++ src/components/My.js
@@ -0,0 +1,40 @@
+const MovieDet = [ // Default Export
+    {
+      Movieame: "Avangers Age of Altron",
+      src: "https://cdn.shopify.com/s/files/1/1148/2822/products/PBMAAOU088.jpg?v=1587408929",
+      Moviecat: "Marvel movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+    {
+      Movieame: "Captain America",
+      src: "https://imgc.allpostersimages.com/img/posters/marvel-cinematic-universe-captain-america-the-first-avenger-one-sheet-premium-poster_u-L-F9KMV60.jpg",
+      Moviecat: "Marvel movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+    {
+      Movieame: "Iroman",
+      src: "https://m.media-amazon.com/images/I/411upOpOjHL._SL500_.jpg",
+      Moviecat: "Marvel movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+    {
+      Movieame: "Superman Man of steel",
+      src: "https://cdn-www.comingsoon.net/assets/uploads/2013/05/file_104542_0_kal-el-poster.jpg",
+      Moviecat: "DC movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+    {
+      Movieame: "The Batman (2022)",
+      src: "https://m.media-amazon.com/images/M/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+      Moviecat: "DC movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+    {
+      Movieame: "WonderWoman",
+      src: "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+      Moviecat: "DC movie",
+      mbtnlink: "https://youtu.be/yEVuK1jYeUo",
+    },
+  ];
+
+  export default MovieDet;
--- src/components/Mycomp.js
+++ src/components/Mycomp.js
@@ -1,9 +1,33 @@
 import React from 'react'
-
-const Mycomp = () => {
-  return (
-    <div>Mycomp</div>
-  )
-}
-
-export default Mycomp
+export const Bollywood = [ //Named Export
+    {
+      Movieame: "Darbar",
+      src: "https://i.pinimg.com/originals/f6/35/fd/f635fda7eac6e9315006ecfba15db2b6.jpg",
+      Moviecat: "Bollywood",
+      mbtnlink: "https://www.youtube.com/watch?v=w0nyEoXBEiA",
+    },
+    {
+      Movieame: "Padmavati",
+      src: "https://qph.cf2.quoracdn.net/main-qimg-b1bd43b2ff2ddd7fc4828555e70a78b4-pjlq",
+      Moviecat: "Bollywood",
+      mbtnlink: "https://www.youtube.com/watch?v=WeYf3CqiJPQ",
+    },
+     {
+      Movieame: "Tanhaji",
+      src: "https://cdnb.artstation.com/p/assets/images/images/024/124/437/large/geethan-punnapully-tanhaji-1.jpg?1581396427",
+      Moviecat: "Bollywood",
+      mbtnlink: "https://www.youtube.com/watch?v=6YcNokBMuRY",
+    },
+     {
+      Movieame: "mohenJodaro",
+      src: "https://static.digit.in/OTT/v2/images/tr:n-ott_home_crousel/mohenjo-daro-35472jpg",
+      Moviecat: "Bollywood",
+      mbtnlink: "https://www.youtube.com/watch?v=UPZ5FKEB02I",
+    },
+    {
+      Movieame: "bajirao mastani",
+      src: "https://m.media-amazon.com/images/M/MV5BODk4OWJlODMtMWYxMS00OWI1LTlkNDMtNGU5OTIyODdmOTc1XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+      Moviecat: "Bollywood",
+      mbtnlink: "https://www.youtube.com/watch?v=eHOc-4D7MjY",
+    },
+  ]
